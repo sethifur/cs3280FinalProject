d65697d3dd6bdee7a82e5a996be9e79bcdc0659c
seth johns <seth johns>
2016-08-03 18:54:45 -0600

    Set up

---
+++ cs3280FinalProject/ViewModels/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs3280FinalProject.ViewModels
+{
+    public class MainViewModel : Conductor<IScreen>.Collection.OneActive
+    {
+        public MainViewModel()
+        {
+            this.DisplayName = "Inventory Application";
+        }
+
+        protected override void OnActivate()
+        {
+            base.OnActivate();
+            Orders();
+        }
+
+        public void Orders()
+        {
+            var ordersVM = IoC.Get<OrdersViewModel>();
+            ActivateItem(ordersVM);
+        }
+    }
+}
